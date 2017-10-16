cab6d312a971ff02ba6caf6d68a1a5bc529a0fa8
Parv Kumar <parvathikumar92@gmail.com>
2017-10-16 23:17:45 +0000

    automatically backed up by learn

--- db/migrate/20171016231623_preference.rb
+++ db/migrate/20171016231623_preference.rb
@@ -1,4 +1,10 @@
 class Preference < ActiveRecord::Migration
   def change
+    create_table :preferences do |t| 
+      t.string song_sort_order
+      t.string artist_sort_order
+      t.boolean allow_create_artists
+      t.boolean allow_create_songs
+    end 
   end
 end
